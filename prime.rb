a77cbed0319cf548a80a7696e49cd8646e22a02a
Otabek Kamalov <otakam86@gmail.com>
2018-07-21 14:42:17 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -3,26 +3,22 @@ def prime? (number)
   if number <= 1 
     
     result = false 
-  
   elsif number == 2 || number == 3 || number == 5 || number== 7
-    
     result = true
-  
   else  
      i = 2
-     
      while i < number 
       
        if number % i == 0 
-      result = false
-      #puts i
-      break
-		    else
-		  	 result = true
-        end 
+          result = false
+          #puts i
+          break
+		   else
+		  	  result = true
+       end 
         	i += 1 
-      	  end 
+     end 
     
- end
+  end
   return result
 end
